morhekil/symfony

commit 60a461a3fd2afd5454a2c6d51fcf9d2a83296f8a

fixed i18n dirs are only extracted from first plugin found - fixes #4829
git-svn-id: file:///Users/saruman/Temp/symfony_svn/branches/1.1@13124 4396de85-42e0-4213-a939-0f47b466bcdf

lib/config/sfApplicationConfiguration.class.php
@@ -403,7 +403,7 @@ abstract class sfApplicationConfiguration extends ProjectConfiguration
    *
    * @return array An array of i18n directories
    */
-  public function getI18NGlobalDirs()
+  public function getDecoratorDirsGlobalDirs()
   {
     $dirs = array();
 
@@ -446,21 +446,12 @@ abstract class sfApplicationConfiguration extends ProjectConfiguration
       $dirs[] = $dir;
     }
 
-    // module in plugins
-    $pluginDirs = glob(sfConfig::get('sf_plugins_dir').'/*/modules/'.$moduleName.'/i18n');
-    if (isset($pluginDirs[0]))
-    {
-      $dirs[] = $pluginDirs[0];
-    }
-
-    // plugins
-    $pluginDirs = glob(sfConfig::get('sf_plugins_dir').'/*/i18n');
-    if (isset($pluginDirs[0]))
-    {
-      $dirs[] = $pluginDirs[0];
-    }
-
-    return $dirs;
+    // plugins, module in plugins
+    return array_merge(
+      $dirs,
+      (array) glob(sfConfig::get('sf_plugins_dir').'/*/modules/'.$moduleName.'/i18n'),
+      (array) glob(sfConfig::get('sf_plugins_dir').'/*/i18n')
+    );
   }
 
   /**

commit 44b5ab982beb419f27d0962e8419abc587c30a9e

[1.1] fixed typo introduced in r13124 (closes #5074)
git-svn-id: file:///Users/saruman/Temp/symfony_svn/branches/1.1@13383 4396de85-42e0-4213-a939-0f47b466bcdf

lib/config/sfApplicationConfiguration.class.php
@@ -403,7 +403,7 @@ abstract class sfApplicationConfiguration extends ProjectConfiguration
    *
    * @return array An array of i18n directories
    */
-  public function getDecoratorDirsGlobalDirs()
+  public function getI18NDirsGlobalDirs()
   {
     $dirs = array();
 

lib/debug/sfTimerManager.class.php
@@ -36,6 +36,8 @@ class sfTimerManager
       self::$timers[$name] = new sfTimer($name);
     }
 
+    self::$timers[$name]->startTimer();
+
     return self::$timers[$name];
   }
 