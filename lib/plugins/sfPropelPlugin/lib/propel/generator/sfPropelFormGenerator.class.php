7ba9312166efdf2930065ab68d1156b6a79792a6
Kris.Wallsmith <Kris.Wallsmith@4396de85-42e0-4213-a939-0f47b466bcdf>
2009-01-14 23:34:36 +0000

    [1.1, 1.2, 1.3] fixed foreign keys are assumed with column name "ID" in auto generation of forms (closes #5370)
    git-svn-id: file:///Users/saruman/Temp/symfony_svn/branches/1.1@14728 4396de85-42e0-4213-a939-0f47b466bcdf

--- lib/plugins/sfPropelPlugin/lib/propel/generator/sfPropelFormGenerator.class.php
+++ lib/plugins/sfPropelPlugin/lib/propel/generator/sfPropelFormGenerator.class.php
@@ -333,16 +333,7 @@ class sfPropelFormGenerator extends sfGenerator
 
     if ($column->isForeignKey())
     {
-      $map = call_user_func(array($this->getForeignTable($column)->getPhpName().'Peer', 'getTableMap'));
-      foreach ($map->getColumns() as $primaryKey)
-      {
-        if ($primaryKey->isPrimaryKey())
-        {
-          break;
-        }
-      }
-
-      $options[] = sprintf('\'model\' => \'%s\', \'column\' => \'%s\'', $this->getForeignTable($column)->getPhpName(), strtolower($primaryKey->getColumnName()));
+      $options[] = sprintf('\'model\' => \'%s\', \'column\' => \'%s\'', $this->getForeignTable($column)->getPhpName(), strtolower($column->getRelatedColumnName()));
     }
     else if ($column->isPrimaryKey())
     {
