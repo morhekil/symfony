bb3a3e5742587000eaca03702468b35db37c19a5
fabien <fabien@4396de85-42e0-4213-a939-0f47b466bcdf>
2009-04-06 13:37:11 +0000

    [1.1, 1.2, 1.3] fixed --csrf-secret and --escaping-strategy options of generate:app
    git-svn-id: file:///Users/saruman/Temp/symfony_svn/branches/1.1@17041 4396de85-42e0-4213-a939-0f47b466bcdf

--- lib/task/generator/sfGenerateAppTask.class.php
+++ lib/task/generator/sfGenerateAppTask.class.php
@@ -118,8 +118,8 @@ EOF;
     $finder = sfFinder::type('file')->name('settings.yml');
     $this->getFilesystem()->replaceTokens($finder->in($appDir.'/config'), '##', '##', array(
       'NO_SCRIPT_NAME'    => $firstApp ? 'on' : 'off',
-      'CSRF_SECRET'       => sfYamlInline::dump($options['csrf-secret']),
-      'ESCAPING_STRATEGY' => sfYamlInline::dump($options['escaping-strategy']),
+      'CSRF_SECRET'       => sfYamlInline::dump(sfYamlInline::parseScalar($options['csrf-secret'])),
+      'ESCAPING_STRATEGY' => sfYamlInline::dump((boolean) sfYamlInline::parseScalar($options['escaping-strategy'])),
     ));
 
     $this->getFilesystem()->copy(dirname(__FILE__).'/skeleton/app/web/index.php', sfConfig::get('sf_web_dir').'/'.$indexName.'.php');
