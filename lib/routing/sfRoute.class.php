123b77c76262b097ce0cc6e84a534af313e405ce
nicolas <nicolas@4396de85-42e0-4213-a939-0f47b466bcdf>
2009-11-18 20:02:03 +0000

    reverted r24146 commited by mistake - so yeah, this was my late evening shame time :-)
    git-svn-id: file:///Users/saruman/Temp/symfony_svn/branches/1.3@24152 4396de85-42e0-4213-a939-0f47b466bcdf

--- lib/routing/sfRoute.class.php
+++ lib/routing/sfRoute.class.php
@@ -314,21 +314,6 @@ class sfRoute implements Serializable
   }
 
   /**
-   * Returns the route parameters.
-   *
-   * @return array The route parameters
-   */
-  public function getParameters()
-  {
-    if (!$this->compiled)
-    {
-      $this->compile();
-    }
-
-    return $this->parameters;
-  }
-
-  /**
    * Returns the compiled pattern.
    *
    * @return string The compiled pattern
